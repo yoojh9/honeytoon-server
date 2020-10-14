7eebf323b30b26ea15941587a96d31da64c4e9cd
yoojh9 <yoojh9@gmail.com>
2020-10-14 13:38:51 +0900

    FCM 연동 테스ㅌ

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:firebase_admob/firebase_admob.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:kakao_flutter_sdk/link.dart';
 import 'package:provider/provider.dart';
@@ -31,6 +32,7 @@ import './providers/my_provider.dart';
 import './providers/comment_provider.dart';
 import './providers/point_provider.dart';
 
+final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -42,9 +44,21 @@ void main() async{
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
 
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  void initState() {
+    super.initState();
+    firebaseCloudMessagingListeners();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
@@ -111,6 +125,36 @@ class MyApp extends StatelessWidget {
         },
       ),
     );
+  }
+
+  void firebaseCloudMessagingListeners() {
+    if(Platform.isIOS) iOSPermission();
+
+    _firebaseMessaging.getToken().then((token){
+      print('token:'+token);
+    });
+
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print('on message $message');
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print('on resume $message');
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print('on launch $message');
+      }
+    );
+
+  }
+
+  void iOSPermission(){
+    _firebaseMessaging.requestNotificationPermissions(
+      IosNotificationSettings(sound: true, badge: true, alert: true)
+    );
+    _firebaseMessaging.onIosSettingsRegistered.listen((IosNotificationSettings settings) {
+      print('Settings registered : $settings');
+    });
   }
 }
 
